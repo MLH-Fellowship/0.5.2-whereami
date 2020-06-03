41c6f14cd5ba9e7bd2ad908eab9d007b87895059
Chris N <hello@chris-nguyen.me>
2020-06-03 15:39:44 -0400

    Flask skeleton webpage (#22)
    * Initial commit
    
    * Skeleton webpage
    
    * hotfixes
    
    * here you go stupid linter
    
    * Updated main.py
    
    Linter complaints satiated

--- web/main.py
+++ web/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/anything')
+def default():
+    return redirect(url_for('index'))
