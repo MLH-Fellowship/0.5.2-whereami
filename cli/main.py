7ab975cfad0b83cce79925ca54f1e1cb8a05b6fe
Yanlam Ko <43295248+YKo20010@users.noreply.github.com>
2020-06-03 16:48:44 -0400

    Add commands for CLI (#23)
    * Update cli main and requirements
    
    * Add single and reverse commands
    
    * Update spacing
    
    * Address PR changes
    
    * Add get_olc_array

--- cli/main.py
+++ cli/main.py
@@ -1,4 +1,4 @@
-from helper import olc_to_phrase, two_to_word
+import helper
 
 import typer
 
@@ -8,21 +8,43 @@ app = typer.Typer()
 
 @app.command()
 def get_word(char_code: str):
-    word = two_to_word(char_code)
+    word = helper.get_word(char_code)
     typer.echo(f"{word}")
 
 
 @app.command()
-def get_phrase(code: str):
+def get_code(word: str):
+    code = helper.get_code(word)
+    typer.echo(f"{code}")
+
+
+@app.command()
+def get_phrase(olc: str, single: bool = False):
     try:
-        if len(code) != 12:
-            error = "Incorrect OLC length. Format: 6PH57VP3+PR6"
-            typer.echo(typer.style(error, fg=typer.colors.RED))
-            return
-        phrase = olc_to_phrase(code)
+        phrase = helper.olc_to_phrase(olc, single)
         typer.echo(f"{phrase}")
     except KeyError:
-        error = typer.style("Not a valid OLC.", fg=typer.colors.RED)
+        error = typer.style("Not a valid plus code.", fg=typer.colors.RED)
+        typer.echo(error)
+
+
+@app.command()
+def get_single_phrase(olc: str):
+    get_phrase(olc, True)
+
+
+@app.command()
+def get_double_phrase(olc: str):
+    get_phrase(olc, False)
+
+
+@app.command()
+def get_plus_code(phrase: str):
+    try:
+        olc = helper.phrase_to_olc(phrase)
+        typer.echo(f"{olc}")
+    except KeyError:
+        error = typer.style("Plus code not found.", fg=typer.colors.RED)
         typer.echo(error)
 
 
--- helper.py
+++ helper.py
@@ -1,15 +1,47 @@
 from functools import reduce
 
-from mappings import map
+from mappings import map, word_to_plus_code_mapping
 
 
-def two_to_word(code: str):
+sep = '.'
+
+
+def get_word(code: str):
     return map[code]
 
 
-def olc_to_phrase(olc: str):
-    code = olc[:-4]
+def get_code(word: str):
+    return word_to_plus_code_mapping[word]
+
+
+def get_olc_array(olc: str, inc: int):
+    code = olc.replace('+', '')
+    return [code[i: i+inc] for i in range(0, len(code), inc)]
+
+
+def single_char_phrase(olc: str):
+    char_arr = get_olc_array(olc, 1)
+    return reduce(lambda acc, s: acc + sep + get_word(s), char_arr, '')[1:]
+
+
+def double_char_phrase(olc: str):
     # Split string into array of 2 char elements.
-    char_arr = [code[i: i+2] for i in range(0, len(code), 2)]
+    char_arr = get_olc_array(olc, 2)
     # Convert coded array to word phrase.
-    return reduce(lambda acc, s: acc + "." + two_to_word(s), char_arr, "")[1:]
+    return reduce(lambda acc, s: acc + sep + get_word(s), char_arr, '')[1:]
+
+
+def olc_to_phrase(olc: str, single: bool = False):
+    if single:
+        return single_char_phrase(olc)
+    else:
+        return double_char_phrase(olc)
+
+
+def phrase_to_olc(phrase: str):
+    word_arr = phrase.split(sep)
+    code_arr = [get_code(word_arr[w]) for w in range(0, len(word_arr))]
+    code = reduce(lambda acc, c: acc + c, code_arr, '')
+    if len(code) % 2 == 0:
+        return code[:-2] + '+' + code[-2:]
+    return code[:-3] + '+' + code[-3:]
