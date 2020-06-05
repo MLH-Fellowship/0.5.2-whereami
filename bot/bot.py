bbaa44f330853fdc7c653d04fdb799bcc8befd62
Kai Chen <kaichen120@gmail.com>
2020-06-04 22:56:15 -0700

    Fix CI checks

--- bot/bot.py
+++ bot/bot.py
@@ -6,22 +6,30 @@ client = discord.Client()
 cmd1 = '$olc'
 cmd2 = '$phrase'
 
+
 @client.event
 async def on_ready():
     print('Logged in successfully.')
 
+
 @client.event
 async def on_message(message):
     if message.content.startswith(cmd1):
-        olc = message.content[len(cmd1):] # rest of code
-        req = requests.get('http://localhost:8000/lookup_olc?olc=' + olc) # http://localhost:8000/lookup_olc?olc=849VCWC8+R9
+        olc = message.content[len(cmd1):]  # rest of code
+        req = requests.get(
+            'http://localhost:8000/lookup_olc?olc=' + olc)
         json = req.json()
-        await message.channel.send(olc + "'s phrase is: " + '**' + json['phrase'] + '**')
+        await message.channel.send(olc + "'s phrase is: " + '**'
+                                   + json['phrase'] + '**')
 
     if message.content.startswith(cmd2):
-        phrase = message.content[len(cmd2):] # rest of phrase
-        req = requests.get('http://localhost:8000/lookup_phrase?phrase=white.horse.body.girl.stead') # http://localhost:8000/lookup_phrase?phrase=white.horse.body.girl.stead
+        phrase = message.content[len(cmd2):]  # rest of phrase
+        req = requests.get(
+            'http://localhost:8000/lookup_phrase?'
+            'phrase=white.horse.body.girl.stead')
         json = req.json()
-        await message.channel.send(phrase + "'s OLC is: " + '**' + json['olc'] + '**')
+        await message.channel.send(phrase + "'s OLC is: " + '**'
+                                   + json['olc'] + '**')
+
 
 client.run('INSERT-TOKEN-HERE')
