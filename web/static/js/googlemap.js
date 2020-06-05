b3600613236b9f7b313dc7610a5f03a6c337a6c4
Kai Chen <kaichen120@gmail.com>
2020-06-04 22:20:57 -0700

    Add Procfile for heroku deploy

--- web/static/js/googlemap.js
+++ web/static/js/googlemap.js
@@ -39,35 +39,33 @@ function initAutocomplete() {
 
         // For each place, get the icon, name and location.
         var bounds = new google.maps.LatLngBounds();
-        places.forEach(function(place) {
-            console.log(place);
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry ");
-                return;
-            }
-            callPlusCodesApi(place.geometry.location)
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
+        let place = places[0];
+        if (!place.geometry) {
+            console.log("Returned place contains no geometry ");
+            return;
+        }
+        callPlusCodesApi(place.geometry.location)
+        var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                title: place.name,
-                position: place.geometry.location
-            }));
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+            map: map,
+            title: place.name,
+            position: place.geometry.location
+        }));
 
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
+        if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
         map.fitBounds(bounds);
     });
 }
@@ -75,15 +73,16 @@ function initAutocomplete() {
 function callPlusCodesApi(bounds) {
     // TODO: THIS IS BASICALLY CALLBACK HELL BUT W/ PROMISES
     fetch(`https://plus.codes/api?address=${bounds.lat()},${bounds.lng()}&ekey=AIzaSyCocZZTCz9pr5gtn5Yx69pG-h4zdwtMBU4&language=en`)
-    .then(res => res.json())
-    .then(json => {
-        let plus_code = json.plus_code.global_code;
-        updatePlusCode(plus_code);
-        // TODO: extract into function
-        fetch(`${baseUrl}/lookup_olc?olc=${plus_code}`)
         .then(res => res.json())
         .then(json => {
-          updateWordPhrase(json.phrase)
+            let plus_code = json.plus_code.global_code;
+            updatePlusCode(plus_code);
+            // TODO: extract into function
+            fetch(`${baseUrl}/lookup_olc?olc=${plus_code}`)
+                .then(res => res.json())
+                .then(json => {
+                    updateWordPhrase(json.phrase)
+                })
         })
-    })
         .catch(err => console.error(err));
+}
